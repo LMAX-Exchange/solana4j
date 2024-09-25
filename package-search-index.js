fe373fba3925dcd3497927d4d01ae11e1d4ed139
ml-james <55996732+ml-james@users.noreply.github.com>
2024-09-25 21:54:39 +0000

    Deploying to gh-pages from @ LMAX-Exchange/solana4j@4f98e25fd6ccd3945ee29ca46b6c497c6904cda0 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.lmax.solana4j"},{"l":"com.lmax.solana4j.api"},{"l":"com.lmax.solana4j.encoding"},{"l":"com.lmax.solana4j.programs"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.lmax.solana4j"},{"l":"com.lmax.solana4j.api"},{"l":"com.lmax.solana4j.encoding"},{"l":"com.lmax.solana4j.programs"}]
