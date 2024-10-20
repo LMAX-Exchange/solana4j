32d02ebffb459cd2cacd75f88151b7f324d2b013
matthewjames <matthew.james@lmax.com>
2024-10-20 12:40:23 -0400

    Does file mounting at least work on the github runner?

--- shared/src/test-support/java/com/lmax/solana4j/shared/IntegrationTestBase.java
+++ shared/src/test-support/java/com/lmax/solana4j/shared/IntegrationTestBase.java
@@ -1,15 +1,17 @@
 package com.lmax.solana4j.shared;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public abstract class IntegrationTestBase
@@ -23,7 +25,6 @@ public abstract class IntegrationTestBase
 
     static
     {
-
         try
         {
             final String arch = System.getProperty("os.arch");
@@ -31,34 +32,31 @@ public abstract class IntegrationTestBase
             // unfortunately have to do this because of a bug in docker-java when using files from resources of another jar
             final Path parentDirectory = Files.createTempDirectory("integrationTests");
             final Path dockerfilePath = copyResourceToTempFile(parentDirectory, arch.equals("aarch64") ? "Aarch64Dockerfile" : "Dockerfile");
-            final Path solanaRunPath = copyResourceToTempFile(parentDirectory, "solana-run.sh");
-            final Path fetchSplPath = copyResourceToTempFile(parentDirectory, "fetch-spl.sh");
-            final Path lmaxMultisigPath = copyResourceToTempFile(parentDirectory, "lmax_multisig.so");
-            final Path upgradeAuthorityPath = copyResourceToTempFile(parentDirectory, "upgrade_authority.json");
-            final Path bpfProgramPath = copyResourceToTempFile(parentDirectory, "bpf_program.json");
 
-            SOLANA_VALIDATOR = new GenericContainer<>(new ImageFromDockerfile().withDockerfile(dockerfilePath))
-                    .withCopyFileToContainer(MountableFile.forHostPath(solanaRunPath), "/solana-run.sh")
-                    .withCopyFileToContainer(MountableFile.forHostPath(fetchSplPath), "/fetch-spl.sh")
-                    .withCopyFileToContainer(MountableFile.forHostPath(lmaxMultisigPath), "/lmax_multisig.so")
-                    .withCopyFileToContainer(MountableFile.forHostPath(upgradeAuthorityPath), "/upgrade_authority.json")
-                    .withCopyFileToContainer(MountableFile.forHostPath(bpfProgramPath), "/bpf_program.json")
-                    .withExposedPorts(SOLANA_HTTP_PORT, SOLANA_WS_PORT)
-                    .withEnv("SOLANA_RUN_SH_VALIDATOR_ARGS", "--ticks-per-slot=8")
-                    .withNetwork(NETWORK);
+            copyResourceToTempFile(parentDirectory, "solana-run.sh");
+            copyResourceToTempFile(parentDirectory, "fetch-spl.sh");
+            copyResourceToTempFile(parentDirectory, "lmax_multisig.so");
+            copyResourceToTempFile(parentDirectory, "upgrade_authority.json");
+            copyResourceToTempFile(parentDirectory, "bpf_program.json");
 
             if (arch.equals("aarch64"))
             {
                 try
                 {
-                    final Path solanaReleasePath = copyResourceToTempFile(parentDirectory, "solana-release-aarch64-unknown-linux-gnu-1.18.25.tar.bz2");
-                    SOLANA_VALIDATOR.withCopyFileToContainer(MountableFile.forHostPath(solanaReleasePath), "/solana-release-aarch64-unknown-linux-gnu-1.18.25.tar.bz2");
+                    copyResourceToTempFile(parentDirectory, "solana-release-aarch64-unknown-linux-gnu-1.18.25.tar.bz2");
                 }
                 catch (final Exception e)
                 {
                     throw new RuntimeException("Cannot find solana-release-aarch64-unknown-linux-gnu-1.18.25.tar.bz2, are you sure you've run BuildMeAnAarch64CompliantSolanaDockerImagePleaseDockerfile?");
                 }
             }
+
+            SOLANA_VALIDATOR = new GenericContainer<>(new ImageFromDockerfile().withDockerfile(dockerfilePath))
+                    .withFileSystemBind(parentDirectory.toAbsolutePath().toString(), "/mount", BindMode.READ_ONLY)
+                    .withExposedPorts(SOLANA_HTTP_PORT, SOLANA_WS_PORT)
+                    .withEnv("SOLANA_RUN_SH_VALIDATOR_ARGS", "--ticks-per-slot=8")
+                    .withNetwork(NETWORK)
+                    .withStartupTimeout(Duration.of(10, ChronoUnit.MINUTES));
 
             SOLANA_VALIDATOR.start();
         }
@@ -70,7 +68,7 @@ public abstract class IntegrationTestBase
 
     private static Path copyResourceToTempFile(final Path parent, final String name) throws IOException
     {
-        final Path tempFile = Files.createTempFile(parent, name, "");
+        final Path tempFile = Files.createFile(parent.resolve(name));
         Files.copy(Objects.requireNonNull(IntegrationTestBase.class.getResourceAsStream("/testcontainers/" + name)), tempFile, StandardCopyOption.REPLACE_EXISTING);
         return tempFile;
     }
