6cbbf7ee85b9c48b3fb72cc421dbade1dfc69202
matthewjames <matthew.james@lmax.com>
2024-10-24 21:17:10 -0400

    Add get balance contract integration test

---
+++ client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/GetBalanceContractTest.java
@@ -0,0 +1,26 @@
+package com.lmax.solana4j.client.jsonrpc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// https://solana.com/docs/rpc/http/getbalance
+class GetBalanceContractTest extends SolanaClientIntegrationTestBase
+{
+    @Test
+    void shouldGetBalance()
+    {
+//        {
+//            "jsonrpc" : "2.0",
+//                "result" : {
+//            "context" : {
+//                "apiVersion" : "2.0.14",
+//                        "slot" : 309
+//            },
+//            "value" : 600000
+//        },
+//            "id" : 4
+//        }
+        assertThat(api.getBalance(payerAccount)).isEqualTo(600000L);
+    }
+}
