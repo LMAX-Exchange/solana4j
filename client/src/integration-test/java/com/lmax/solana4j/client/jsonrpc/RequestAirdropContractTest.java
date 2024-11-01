d362d8c9b863f871aa70e63d09a85927ae8be7a2
matthewjames <matthew.james@lmax.com>
2024-11-01 19:35:36 -0400

    Complete RequestAirdropContractTest

--- client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/RequestAirdropContractTest.java
+++ client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/RequestAirdropContractTest.java
@@ -1,30 +1,27 @@
 package com.lmax.solana4j.client.jsonrpc;
 
 import com.lmax.solana4j.domain.Sol;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// https://solana.com/docs/rpc/http/requestairdrop
 class RequestAirdropContractTest extends SolanaClientIntegrationTestBase
 {
     @Test
     void shouldRequestAirdrop() throws SolanaJsonRpcClientException
     {
-//        {
-//            "jsonrpc" : "2.0",
-//                "result" : "cPN2nfGaYtiFC9yR5FFLANmE5jxQFXxyYH3keCPKXuZ4LNAoZBvnqhFLomAMznTGorkw1iMrX3qNy8AVmX8UwEF",
-//                "id" : 4
-//        }
         assertThat(api.requestAirdrop(payerAccount, Sol.lamports(BigDecimal.ONE)).getResponse()).isNotBlank();
     }
 
     @Test
-    @Disabled
-    void shouldThrowRpcExceptionForMalformedAccount()
+    void shouldReturnErrorForMalformedAccount() throws SolanaJsonRpcClientException
     {
-
+        final var response = api.requestAirdrop("iamnotarealaccount", Sol.lamports(BigDecimal.ONE));
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getError().getErrorCode()).isEqualTo(-32602L);
+        assertThat(response.getError().getErrorMessage()).isEqualTo("Invalid param: Invalid");
     }
 }
