d64e09a43d48c1f9afbb9b852273c44a0a290b5b
matthewjames <matthew.james@lmax.com>
2024-11-01 19:41:34 -0400

    Complete GetBlockHeightContractTest

--- client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/GetBlockHeightContractTest.java
+++ client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/GetBlockHeightContractTest.java
@@ -1,20 +1,29 @@
 package com.lmax.solana4j.client.jsonrpc;
 
+import com.lmax.solana4j.client.api.SolanaClientOptionalParams;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// https://solana.com/docs/rpc/http/getblockheight
 class GetBlockHeightContractTest extends SolanaClientIntegrationTestBase
 {
     @Test
     void shouldGetBlockHeight() throws SolanaJsonRpcClientException
     {
-//        {
-//            "jsonrpc" : "2.0",
-//                "result" : 427,
-//                "id" : 4
-//        }
-
         assertThat(api.getBlockHeight().getResponse()).isGreaterThan(0L);
+    }
+
+    @Test
+    void shouldReturnErrorForMinContextSlotNotReached() throws SolanaJsonRpcClientException
+    {
+        final SolanaClientOptionalParams optionalParams = new SolanaJsonRpcClientOptionalParams();
+        optionalParams.addParam("minContextSlot", 10000000000L);
+
+        final var response = api.getBlockHeight(optionalParams);
+
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getError().getErrorCode()).isEqualTo(-32016L);
+        assertThat(response.getError().getErrorMessage()).isEqualTo("Minimum context slot has not been reached");
     }
 }
