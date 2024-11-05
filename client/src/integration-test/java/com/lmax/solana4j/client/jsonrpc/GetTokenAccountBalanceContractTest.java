866d80810e3a48d490c9b35df33cfbd9e552e3c8
matthewjames <matthew.james@lmax.com>
2024-11-04 21:17:59 -0500

    Complete GetTokenAccountBalanceContractTest

--- client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/GetTokenAccountBalanceContractTest.java
+++ client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/GetTokenAccountBalanceContractTest.java
@@ -1,10 +1,10 @@
 package com.lmax.solana4j.client.jsonrpc;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// https://solana.com/docs/rpc/http/gettokenaccountbalance
 class GetTokenAccountBalanceContractTest extends SolanaClientIntegrationTestBase
 {
     @Test
@@ -19,24 +19,32 @@ class GetTokenAccountBalanceContractTest extends SolanaClientIntegrationTestBase
     }
 
     @Test
-    @Disabled
-    void shouldThrowRpcExceptionForNonTokenAccountBalance()
+    void shouldReturnErrorForUnknownAccount() throws SolanaJsonRpcClientException
     {
+        final var response = api.getTokenAccountBalance("4zDkK3Y6HBxzJ9tqWbZ9RG5aPGRJ5B7G4VQV67gHUMoP");
 
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getError().getErrorCode()).isEqualTo(-32602L);
+        assertThat(response.getError().getErrorMessage()).isEqualTo("Invalid param: could not find account");
     }
 
     @Test
-    @Disabled
-    void shouldThrowRpcExceptionForUnknownTokenAccountBalance()
+    void shouldReturnErrorForNonTokenAccount() throws SolanaJsonRpcClientException
     {
+        final var response = api.getTokenAccountBalance(payerAccount);
 
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getError().getErrorCode()).isEqualTo(-32602L);
+        assertThat(response.getError().getErrorMessage()).isEqualTo("Invalid param: not a Token account");
     }
 
     @Test
-    @Disabled
-    void shouldThrowRpcExceptionForMalformedTokenAccountBalance()
+    void shouldReturnErrorForMalformedTokenAccount() throws SolanaJsonRpcClientException
     {
+        final var response = api.getTokenAccountBalance("iamnotarealaccount");
 
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getError().getErrorCode()).isEqualTo(-32602L);
+        assertThat(response.getError().getErrorMessage()).isEqualTo("Invalid param: Invalid");
     }
-
 }
