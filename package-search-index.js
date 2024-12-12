b04403e7bfbbea1c339be723354e41c0201f7237
ml-james <55996732+ml-james@users.noreply.github.com>
2024-12-12 14:46:58 +0000

    Deploying to gh-pages from @ LMAX-Exchange/solana4j@2a72ce08540ba3c86750cdddea5edb1caec8071b 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"com.lmax.solana4j","l":"com.lmax.solana4j"},{"m":"com.lmax.solana4j","l":"com.lmax.solana4j.api"},{"m":"com.lmax.solana4j","l":"com.lmax.solana4j.encoding"},{"m":"com.lmax.solana4j","l":"com.lmax.solana4j.programs"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"com.lmax.solana4j.client","l":"com.lmax.solana4j.client"},{"m":"com.lmax.solana4j.client","l":"com.lmax.solana4j.client.api"},{"m":"com.lmax.solana4j.client","l":"com.lmax.solana4j.client.jsonrpc"}]
