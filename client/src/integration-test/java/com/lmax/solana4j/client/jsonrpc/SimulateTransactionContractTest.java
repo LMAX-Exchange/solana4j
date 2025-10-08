d20b1eedd6d1258b920674a86088173a026589a5
healeyr <healeyr@lmax.com>
2025-10-08 11:43:48 +0100

    Update units consumed in test

--- client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/SimulateTransactionContractTest.java
+++ client/src/integration-test/java/com/lmax/solana4j/client/jsonrpc/SimulateTransactionContractTest.java
@@ -74,7 +74,7 @@ final class SimulateTransactionContractTest extends SolanaClientIntegrationTestB
     {
         final var response = SOLANA_API.simulateTransaction(mintToTransactionBlobBase64).getResponse();
 
-        assertThat(response.getUnitsConsumed()).isEqualTo(958);
+        assertThat(response.getUnitsConsumed()).isEqualTo(1038);
         assertThat(response.getLogs().size()).isEqualTo(4);
         // inner instructions are program invocations that occur inside the execution of a main (outer)
         // transaction instruction
@@ -152,7 +152,7 @@ final class SimulateTransactionContractTest extends SolanaClientIntegrationTestB
 
         final var response = SOLANA_API.simulateTransaction(mintToTransactionBlobBase58, optionalParams);
 
-        assertThat(response.getResponse().getUnitsConsumed()).isEqualTo(958);
+        assertThat(response.getResponse().getUnitsConsumed()).isEqualTo(1038);
         assertThat(response.getResponse().getLogs().size()).isEqualTo(4);
         assertThat(response.getResponse().getErr()).isNull();
     }
